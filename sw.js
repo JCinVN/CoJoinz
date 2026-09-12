57faa4fa2fb4fdc84c31748d9ea9462b86308a38
JCinVN <ycchen1983@gmail.com>
2026-09-12 23:26:18 +0700

    V6

--- sw.js
+++ sw.js
@@ -1,38 +1,18 @@
-const CACHE_NAME = 'cojoinz-hub-v6';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './logo.png',
-  './scorz.png',
-  './wordscanz.png',
-  './infoz.png'
-];
-
+// 每次載入直接清空瀏覽器的所有 Cache Storage
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
+      return Promise.all(keys.map((key) => caches.delete(key)));
     })
   );
   self.clients.claim();
 });
 
+// 完全繞過快取，直接抓取網路最新資料
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
